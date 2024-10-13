371149d00187b27d57a68dd55f775009002d7041
Seth Way <way.sethaniel@gmail.com>
2024-10-12 20:31:12 -0400

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,23 @@
 // vite.config.js in todo-components
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import federation from "@originjs/vite-plugin-federation";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import federation from '@originjs/vite-plugin-federation';
 
 export default defineConfig({
   plugins: [
     react(),
     federation({
-      name: "chess-components",
-      filename: "remoteEntry.js",
+      name: 'chess-components',
+      filename: 'remoteEntry.js',
       exposes: {
-        "./Board": "./src/components/Board.jsx",
+        './Board': './src/components/Board/Board.jsx',
       },
-      shared: ["react"],
+      shared: ['react'],
     }),
   ],
   build: {
     modulePreload: false,
-    target: "esnext",
+    target: 'esnext',
     minify: false,
     cssCodeSplit: false,
   },
