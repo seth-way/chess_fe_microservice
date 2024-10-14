0dd1bc752bba4aa421ab7f3580dd7585f1e16001
Seth Way <way.sethaniel@gmail.com>
2024-10-14 10:17:48 -0400

    add cypress initial tests

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,7 +1,7 @@
 import './Board.css';
 
-const Board = () => {
-  return <div>Chess Board from FE Micro Service</div>;
-};
+function Board() {
+	return <div>Chess Board from FE Micro Service</div>;
+}
 
 export default Board;
--- vite.config.js
+++ vite.config.js
@@ -4,21 +4,21 @@ import react from '@vitejs/plugin-react';
 import federation from '@originjs/vite-plugin-federation';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    federation({
-      name: 'chess-components',
-      filename: 'remoteEntry.js',
-      exposes: {
-        './Board': './src/components/Board/Board.jsx',
-      },
-      shared: ['react'],
-    }),
-  ],
-  build: {
-    modulePreload: false,
-    target: 'esnext',
-    minify: false,
-    cssCodeSplit: false,
-  },
+	plugins: [
+		react(),
+		federation({
+			name: 'chess-components',
+			filename: 'remoteEntry.js',
+			exposes: {
+				'./Board': './src/components/Board/Board.jsx'
+			},
+			shared: ['react']
+		})
+	],
+	build: {
+		modulePreload: false,
+		target: 'esnext',
+		minify: false,
+		cssCodeSplit: false
+	}
 });
