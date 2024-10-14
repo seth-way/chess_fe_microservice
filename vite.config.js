ff2d0a4091f4f46fef6bdddf1113049b9be9e5fd
Seth Way <way.sethaniel@gmail.com>
2024-10-14 17:15:52 -0400

    bug - allow CORS from vite

--- vite.config.js
+++ vite.config.js
@@ -19,6 +19,8 @@ export default defineConfig({
     cors: true, // Enable CORS
     headers: {
       'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
     },
   },
   build: {
