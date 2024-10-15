ab265a370e92386ab647a23f1956eed08e8cb372
Seth Way <way.sethaniel@gmail.com>
2024-10-15 15:34:08 -0400

    update test component text

--- src/components/TestComponent/TestComponent.cy.jsx
+++ src/components/TestComponent/TestComponent.cy.jsx
@@ -4,6 +4,9 @@ describe('<TestComponent />', () => {
   it('renders', () => {
     // see: https://on.cypress.io/mounting-react
     cy.mount(<TestComponent />);
-    cy.get('div').should('contains.text', 'Chess Board from FE Micro Service');
+    cy.get('div').should(
+      'contains.text',
+      'Test component from FE Micro Service'
+    );
   });
 });
