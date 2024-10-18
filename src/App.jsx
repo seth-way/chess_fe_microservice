e41feb8bb7e4c27e99eac82e28b4db9b9b48e4e3
Seth Way <way.sethaniel@gmail.com>
2024-10-18 10:14:24 -0400

    building mock server

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import './App.css';
 import Game from './components/Game/Game';
 
 function App() {
-  return (
-    <>
-    <div className="game-area"></div>
-      <Game />
-    </>
-  );
+	return (
+		<>
+			<div className="game-area"></div>
+			<Game />
+		</>
+	);
 }
 
 export default App;
