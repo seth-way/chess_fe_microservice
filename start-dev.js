d8ef28873b6c7e18811660feed496849d74b7e7e
Seth Way <way.sethaniel@gmail.com>
2024-10-19 16:23:35 -0400

    clean up start dev script

--- start-dev.js
+++ start-dev.js
@@ -1,30 +1,22 @@
-// Import 'dotenv' to load environment variables
 import dotenv from 'dotenv';
 import { spawn } from 'child_process';
 
-// Load environment variables from .env file
 dotenv.config();
-
-console.log('run m s', process.env.RUN_MOCK_SERVER);
-
-// Check if the environment variable RUN_SERVER is set to true
+// eslint-disable-next-line no-undef
 const runServer = process.env.RUN_MOCK_SERVER === 'true';
 
-// Function to run a command
 const runCommand = (command, args) => {
-	const process = spawn(command, args, { stdio: 'inherit' });
-	process.on('close', code => {
-		if (code !== 0) {
-			console.error(`${command} process exited with code ${code}`);
-		}
-	});
+  const process = spawn(command, args, { stdio: 'inherit' });
+  process.on('close', code => {
+    if (code !== 0) {
+      console.error(`${command} process exited with code ${code}`);
+    }
+  });
 };
 
-// Run the server if the environment variable is true
 if (runServer) {
-	console.log('Running Mock Server.');
-	runCommand('node', ['./mockServer/server.js']);
+  console.log('Running Mock Server.');
+  runCommand('node', ['./mockServer/server.js']);
 }
 
-// Always run Vite
 runCommand('vite', []);
