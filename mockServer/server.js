6cfb8657adb344f6d28aa4bb1a5add58d3db9f47
Seth Way <way.sethaniel@gmail.com>
2024-10-19 16:08:18 -0400

    update mock server

--- mockServer/server.js
+++ mockServer/server.js
@@ -73,32 +73,3 @@ function sendLatest(game, socket) {
 
   socket.send(JSON.stringify(data));
 }
-
-// function makeAMove(move) {
-//   const gameCopy = { ...game };
-//   const result = gameCopy.move(move);
-//   setGame(gameCopy);
-//   return result;
-// }
-
-// function makeRandomMove() {
-//   const possibleMoves = game.moves();
-//   if (game.game_over() || game.in_draw() || possibleMoves.length === 0)
-//     return;
-//   const randomIndex = Math.floor(Math.random() * possibleMoves.length);
-//   makeAMove(possibleMoves[randomIndex]);
-// }
-
-// function onDrop(sourceSquare, targetSquare) {
-//   const move = makeAMove({
-//     from: sourceSquare,
-//     to: targetSquare,
-//     promotion: 'q', // always promote to a queen for example simplicity
-//   });
-
-//   // illegal move
-//   if (move === null) return false;
-//   console.log(game.fen());
-//   setTimeout(makeRandomMove, 200);
-//   return true;
-// }
