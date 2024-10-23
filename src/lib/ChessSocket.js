929b5fbe307f2efbedf2560063644b3c8b24450b
Seth Way <way.sethaniel@gmail.com>
2024-10-23 18:18:38 -0400

    merge local ws changes with updates from remote

--- src/lib/ChessSocket.js
+++ src/lib/ChessSocket.js
@@ -1,8 +1,10 @@
 import { io } from 'socket.io-client';
 
 export default function ChessSocket(gameId) {
-  // const socket = io('https://chess-game-be-fmpc.onrender.com/', {
-  const socket = io('localhost:5000/', {
+  //// for deployed BE
+  const socket = io('https://chess-game-be-fmpc.onrender.com/', {
+  //// for local BE
+  //const socket = io('localhost:5000/', {
     transports: ['websocket'], // Force the use of WebSockets only
     query: {
       gameId,
