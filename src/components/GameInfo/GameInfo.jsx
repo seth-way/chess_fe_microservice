dd664ae0c049503e411ccc5d3721c0d9f6bdaff6
Gwyneth Patrick <gwynethpatrick235@gmail.com>
2024-10-24 12:48:17 -0600

    refacttor: new victory message.

--- src/components/GameInfo/GameInfo.jsx
+++ src/components/GameInfo/GameInfo.jsx
@@ -1,14 +1,14 @@
 import './GameInfo.css'
 function GameInfo({turnColor, opponentName, outcome, champion, turnNumber}){
     //uncomment below to manually test gameStates other than turn color.
-        // complete = true
-        // draw = true
+        // outcome = 'draw'
+        // outcome = 'checkmate'
         // champion = 'Player 1'
     let gameState;
     if (outcome){
         outcome === 'draw'?
         gameState = 'Game complete: draw.'
-        :gameState = `Game complete: ${champion} won.`;
+        :gameState = `Game complete by ${outcome}, ${champion} won.`;
     } else {
         gameState = `Current turn: ${turnNumber}, ${turnColor} to move.`;
     };
